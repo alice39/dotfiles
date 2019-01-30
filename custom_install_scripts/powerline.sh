b40750848b29238e59ae1600f04be60913985289
Hans Goor <RootedDev@outlook.com>
2019-01-30 19:41:34 +0100

    Fixed powerline bug

--- custom_install_scripts/powerline.sh
+++ custom_install_scripts/powerline.sh
@@ -10,8 +10,6 @@ echo Creating powerline directory...
 mkdir -p ~/.powerline/
 
 echo Moving required files...
-cp scripts/powerline-daemon ~/.powerline/
+cp -r . ~/.powerline/
 
-# Move the zsh binding to the same folder.
-cp powerline/bindings/zsh/powerline.zsh ~/.powerline/
 echo Done!
