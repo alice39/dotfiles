07c381dfc4eabf0e56f44a8159548dac1bac45ff
Hans Goor <rooteddev@outlook.com>
2019-03-06 15:23:47 +0100

    Updated Polybar to launch on all monitors. Added wallpapers

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,6 +6,8 @@ killall -q polybar
 # Wait until the processes have been killed.
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# Launch polybar.
-polybar top &
-polybar bottom &
+# Launch polybar on all monitors.
+for m in $(polybar --list-monitors | cut -d ":" -f1); do
+  MONITOR=$m polybar top &
+  MONITOR=$m polybar bottom &
+done
