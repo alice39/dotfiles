e820159d667072a93a77ab1bd6f316aedea3198c
Hans Goor <rooteddev@outlook.com>
2019-05-21 11:05:01 +0200

    Fixed volume not playing sound

--- config/i3/post-init.sh
+++ config/i3/post-init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 /usr/bin/xrandr --output eDP1 --mode 1920x1200 --primary
+/usr/bin/xss-lock pactl set-sink-volume @DEFAULT_SINK@ 0 &
 /usr/bin/light-locker --lock-on-suspend --no-idle-hint
