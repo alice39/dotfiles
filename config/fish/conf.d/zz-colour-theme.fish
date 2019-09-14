3b86b78fe8a46517cbc8cd9bc4ef4e9b529b8811
Hans Goor <rooteddev@outlook.com>
2019-09-14 22:43:19 +0200

    Added a fish installer

---
+++ config/fish/conf.d/zz-colour-theme.fish
@@ -0,0 +1,3 @@
+# Import PyWal colour scheme
+cat ~/.cache/wal/sequences &
+bass source ~/.cache/wal/colors-tty.sh
---
+++ config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/eyedevelop/.local/share/omf/themes/agnoster/fish_prompt.fish
