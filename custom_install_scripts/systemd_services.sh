57645cf6c1799bee896b321b311417a08887b559
Hans Goor <rooteddev@outlook.com>
2019-09-15 14:55:52 +0200

    Auto enable systemd services. Also create self-made service files.

---
+++ custom_install_scripts/systemd_services.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Install custom service files.
+cat << EOF > /etc/systemd/system/powertop-tunables.service
+[Unit]
+Description=PowerTOP Auto Tune
+
+[Service]
+Type=idle
+Environment="TERM=dumb"
+ExecStart=/usr/bin/powertop --auto-tune
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat << EOF > /etc/systemd/system/lockscreen.service
+[Unit]
+Description=Broadcast lockscreen time
+Before=sleep.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/loginctl lock-sessions
+
+[Install]
+WantedBy=sleep.target
+EOF
+
+# Enable services.
+sudo systemctl enable NetworkManager
+sudo systemctl enable gdm
+sudo systemctl enable tlp
+sudo systemctl enable tlp-sleep
+sudo systemctl enable org.cups.cupsd
+sudo systemctl enable powertop-tunables
+sudo systemctl enable lockscreen
+
+# Enable user services.
+systemctl enable --user parsecd
