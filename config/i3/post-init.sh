6ce708d4f374c5796a54e3894241c20739c4d415
Hans Goor <rooteddev@outlook.com>
2019-09-22 16:55:41 +0200

    Mute speaker on lock and changed wallpaper definition

--- config/i3/post-init.sh
+++ config/i3/post-init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 /usr/bin/xrandr --output eDP1 --mode 1920x1200 --primary
-/usr/bin/xss-lock pactl set-sink-volume @DEFAULT_SINK@ 0 &
+/usr/bin/xss-lock pactl set-sink-volume 0 0 &
 /usr/bin/light-locker --lock-on-suspend --no-idle-hint
