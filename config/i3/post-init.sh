11b8140257cf679a48e5ff50aa83074e0488e1c5
Hans Goor <rooteddev@outlook.com>
2019-09-25 18:36:22 +0200

    Replaced light locker with betterlockscreen

--- config/i3/post-init.sh
+++ config/i3/post-init.sh
@@ -2,4 +2,4 @@
 
 /usr/bin/xrandr --output eDP1 --mode 1920x1200 --primary
 /usr/bin/xss-lock pactl set-sink-volume 0 0 &
-/usr/bin/light-locker --lock-on-suspend --no-idle-hint
+/usr/bin/xss-lock betterlockscreen --lock blur &
