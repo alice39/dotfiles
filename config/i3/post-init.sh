33b2f458af0205d45f1c385cd80cb641959a8543
Hans Goor <rooteddev@outlook.com>
2019-10-03 15:32:00 +0200

    Updated i3 config and screenshot binding.

--- config/i3/post-init.sh
+++ config/i3/post-init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 /usr/bin/xrandr --output eDP1 --mode 1920x1200 --primary
-/usr/bin/xss-lock pactl set-sink-volume 0 0 &
-/usr/bin/xss-lock betterlockscreen --lock blur &
+/usr/bin/xss-lock -- pactl set-sink-volume 0 0 &
+/usr/bin/xss-lock -- betterlockscreen --lock blur &
