4ebbce7682c0281c15bf9517ff43cb3e0798a993
Hans Goor <rooteddev@outlook.com>
2019-10-21 20:55:18 +0200

    Fixed some issues

--- custom_install_scripts/oh-my-fish.sh
+++ custom_install_scripts/oh-my-fish.sh
@@ -2,4 +2,4 @@
 
 curl -L https://get.oh-my.fish -o /tmp/install_omf && chmod 755 /tmp/install_omf && fish /tmp/install_omf --noninteractive
 chsh -s /usr/bin/fish $(whoami)
-fish -c "omf install agnoster"
+fish -c "omf install agnoster bass"
