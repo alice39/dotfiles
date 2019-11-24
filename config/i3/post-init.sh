a0b951e8567491206e11e20556f07f7bd29f08ff
Hans Goor <rooteddev@outlook.com>
2019-11-24 18:38:05 +0100

    Changed the dispatcher for more accuracy.

--- config/i3/post-init.sh
+++ config/i3/post-init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+/usr/bin/xautolock -time 15 -locker "systemctl suspend" &
 /usr/bin/xss-lock -- $HOME/.config/i3/locker.sh
--- config/nm_dispatcher/get-public-ip.sh
+++ config/nm_dispatcher/get-public-ip.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Check if the dispatcher action is the right one.
-#if [[ "$NM_DISPATCHER_ACTION" == "connectivity-change" ]]; then
-    # Let the network connection settle.
-    sleep 1
+# Let the network connection settle.
+sleep 5
 
-    # Get the new public ip address.
-    # ip=$(dig +short myip.opendns.com @resolver1.opendns.com)  # <-- Using DNS (faster, but could be blocked)
-    ip=$(curl 'http://ipecho.net/plain')  # <-- Using HTTP (slower, but not blocked)
+# Get the new public ip address.
+# ip=$(dig +short myip.opendns.com @resolver1.opendns.com)  # <-- Using DNS (faster, but could be blocked)
+ip=$(curl 'http://ipecho.net/plain')  # <-- Using HTTP (slower, but not blocked)
 
-    # Check if it ran successfully.
-    [[ "$?" -ne 0 ]] && ip="-"
+# Check if it ran successfully.
+[[ "$?" -ne 0 ]] && ip="-"
 
-    # Write the ip to a file.
-    echo "$ip" > /run/publicip
-#fi
+# Write the ip to a file.
+echo "$ip" > /run/publicip
