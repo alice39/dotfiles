e7987c0f995c7c8aa507d14216e3a7c226bf3534
Yuhanun <y.citgez@student.utwente.nl>
2019-11-25 16:31:02 +0100

    Initial commit

---
+++ fish/lock.fish
@@ -0,0 +1,6 @@
+function lock
+	rm -rf $HOME/.locker/no_blur_lock.png
+	scrot $HOME/.locker/no_blur_lock.png
+	convert $HOME/.locker/no_blur_lock.png -blur 0x8 $HOME/.locker/lock.png
+	i3lock -i $HOME/.locker/lock.png
+end
---
+++ fish/wake_pc.fish
@@ -0,0 +1,3 @@
+function wake_pc
+	wol --ipaddr=john.student.utwente.nl B4:2E:99:35:BB:C8;
+end
