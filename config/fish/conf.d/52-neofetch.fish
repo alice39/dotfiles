fb26c7c6327cfb670685b86b3ff052a7baeb415c
Hans Goor <rooteddev@outlook.com>
2019-12-10 21:10:32 +0100

    Switched neofetch for pfetch

--- config/fish/conf.d/52-neofetch.fish
+++
@@ -1,2 +0,0 @@
-# Start neofetch when running interactively.
-status --is-interactive; and neofetch --ascii ~/.config/fish/lipsum.txt
---
+++ config/fish/conf.d/52-pfetch.fish
@@ -0,0 +1,2 @@
+# Start neofetch when running interactively.
+status --is-interactive; and pfetch
