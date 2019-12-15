9c29458691a0e02550b1e69f8a91455875dbbf64
Hans Goor <rooteddev@outlook.com>
2019-12-15 23:43:25 +0100

    Updated polybar networking to no longer be device specific.

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,6 +8,6 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch polybar on all monitors.
 for m in $(polybar --list-monitors | cut -d ":" -f1); do
-  MONITOR=$m polybar top &
+  MONITOR=$m WIRELESS_IFACE=$(ip link | grep wlp | awk '{print $2}' | tr -d ':') WIRED_IFACE=$(ip link | grep enp | awk '{print $2}' | tr -d ':') polybar top &
   MONITOR=$m polybar bottom &
 done
