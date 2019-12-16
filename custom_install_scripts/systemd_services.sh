d48b42ab4a1b59f8654d45478ac262bab334164c
Hans Goor <rooteddev@outlook.com>
2019-12-16 22:51:30 +0100

    Powertop tunables interferes with TLP.

--- custom_install_scripts/systemd_services.sh
+++ custom_install_scripts/systemd_services.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
 # Install custom service files.
-cat << EOF | sudo tee /etc/systemd/system/powertop-tunables.service
-[Unit]
-Description=PowerTOP Auto Tune
-
-[Service]
-Type=idle
-Environment="TERM=dumb"
-ExecStart=/usr/bin/powertop --auto-tune
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
 cat << EOF | sudo tee /etc/systemd/system/lockscreen.service
 [Unit]
 Description=Broadcast lockscreen time
@@ -33,7 +20,6 @@ sudo systemctl enable gdm
 sudo systemctl enable tlp
 sudo systemctl enable tlp-sleep
 sudo systemctl enable org.cups.cupsd
-sudo systemctl enable powertop-tunables
 sudo systemctl enable lockscreen
 
 # Enable user services.
