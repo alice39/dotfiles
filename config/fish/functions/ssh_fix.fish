ac6423af99a360a7a1304752a3f6e359989af130
Hans Goor <rooteddev@outlook.com>
2019-12-21 16:33:29 +0100

    Added a function to fix terminfo stuff.

---
+++ config/fish/functions/ssh_fix.fish
@@ -0,0 +1,9 @@
+function ssh_fix --description "Fixes the xterm-kitty being unknown."
+    if test (count $argv) -lt 1
+        echo "Specify the SSH args please."
+        return 1
+    end
+
+    infocmp xterm-kitty | ssh $argv tic -x -o \~/.terminfo /dev/stdin
+    echo "Done!"
+end
