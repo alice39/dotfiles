c5ba6e4ede26efc846759fef710fb8b7a4384f18
Hans Goor <rooteddev@outlook.com>
2020-02-23 21:40:04 +0100

    Removed TLP from auto install.

--- custom_install_scripts/systemd_services.sh
+++ custom_install_scripts/systemd_services.sh
@@ -18,10 +18,5 @@ EOF
 sudo systemctl enable NetworkManager
 sudo systemctl enable NetworkManager-dispatcher
 sudo systemctl enable gdm
-sudo systemctl enable tlp
-sudo systemctl enable tlp-sleep
 sudo systemctl enable org.cups.cupsd
 sudo systemctl enable lockscreen
-
-# Enable user services.
-systemctl enable --user parsecd
