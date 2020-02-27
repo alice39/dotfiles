9527d5753403fdd69581f7f61e8965cc5ebd5b4b
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-02-27 02:57:06 +0000

    ycm can't be done because PluginInstall must happen first

--- custom_install_scripts/vim.sh
+++ custom_install_scripts/vim.sh
@@ -2,3 +2,5 @@
 
 [ ! -d ~/.vim/bundle/Vundle.vim ]  && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -n $PWD/config/vimrc ~/.vimrc
+
+echo "Go into VIM, type :PluginInstall, then compile ycm""
