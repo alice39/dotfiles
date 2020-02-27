7a9ad4214860f2f8e0cb37979ddba6148ccd5917
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-02-27 02:56:01 +0000

    VIM setup done, mostly, working on ycm

--- custom_install_scripts/vim.sh
+++ custom_install_scripts/vim.sh
@@ -1,1 +1,4 @@
-mv $PWD/config/vimrc ~/.vimrc
+#!/bin/bash
+
+[ ! -d ~/.vim/bundle/Vundle.vim ]  && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -n $PWD/config/vimrc ~/.vimrc
