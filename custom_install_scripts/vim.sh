35a07dbe9d1158ba218570ae428bef97f6a235ad
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-04-10 22:07:32 +0200

    Added kite

--- custom_install_scripts/vim.sh
+++ custom_install_scripts/vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Install kite: https://kite.com/linux/"
+bash -c "$(wget -q -O - https://linux.kite.com/dls/linux/current)"
 [ ! -d ~/.vim/bundle/Vundle.vim ]  && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -n $PWD/config/vimrc ~/.vimrc
 yay -S kite
