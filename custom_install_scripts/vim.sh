36dd7f3f5fb77274f0ca0fbe22a6c0469d8f4b7d
Yuhanun Citgez <y.citgez@student.utwente.nl>
2020-04-10 21:55:13 +0200

    Updated version

--- custom_install_scripts/vim.sh
+++ custom_install_scripts/vim.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-yay -S rust npm go jdk8-openjdk mono typescript --needed
+echo "Install kite: https://kite.com/linux/"
 [ ! -d ~/.vim/bundle/Vundle.vim ]  && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -n $PWD/config/vimrc ~/.vimrc
-vim -c ":PluginInstall"
-cwd=$PWD
-cd ~/.vim/bundle/youcompleteme
-./install.py --all --clang-completer
+yay -S kite
